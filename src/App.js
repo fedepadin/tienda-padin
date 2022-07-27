d393d1125bb0da765be4516f93a81ab059a53fd3
unknown <fedepadin98@gmail.com>
2022-07-26 21:45:08 -0300

    Segunda entrega

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import NavBar from './Components/NavBar';
+import "./estilo.css";
+
+
+
+function App() {
+  return (
+
+    <NavBar></NavBar>
+    
+  );
+
+}
+
+export default App;
---
+++ src/Components/NavBar.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+
+function NavBar(){
+
+    return (
+
+        <header>
+
+            <h2>SHOPPING ONLINE</h2>
+
+           <nav id="navbar">
+            <a className="navLink" href="#">INICIO</a>
+            <a className="navLink" href="#">NOSOTROS</a>
+            <a className="navLink" href="#">PRODUCTOS</a>
+            <a className="navLink" href="#">CONTACTO</a>
+
+            </nav> 
+
+        </header>
+
+    );
+}
+
+export default NavBar;
